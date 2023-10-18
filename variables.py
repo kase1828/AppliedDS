e0785058e43b3fcea61bfe0843507d16c1b17df2
Kasper Seglem <kasperseglem@dhcp-10-22-116-61.wlan.ntnu.no>
2023-10-18 15:25:58 +0200

    Clean up code

--- variables.py
+++ variables.py
@@ -1,14 +1,19 @@
 # Author: Kasper Seglem
 
+# Imports
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
 # Load the data
 data = pd.read_csv("dropout.csv", delimiter=";")
 
 # Check for missing values
 missing_values = data.isnull().sum()
-
-missing_values
 
 # Separate target from predictors
 X = data.drop("Target", axis=1)
@@ -18,9 +23,6 @@ y = data["Target"]
 categorical_cols = X.select_dtypes(include=["object", "category"]).columns.tolist()
 numerical_cols = X.select_dtypes(exclude=["object", "category"]).columns.tolist()
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-
 # Split data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -29,15 +31,11 @@ scaler = StandardScaler()
 X_train[numerical_cols] = scaler.fit_transform(X_train[numerical_cols])
 X_test[numerical_cols] = scaler.transform(X_test[numerical_cols])
 
-from sklearn.linear_model import LogisticRegression
-import numpy as np
-
 # Train a logistic regression model
 model = LogisticRegression(max_iter=1000, random_state=42)
 model.fit(X_train, y_train)
 
 # Get feature importances
-# For logistic regression, the magnitude of the coefficients can be interpreted as feature importance
 feature_importances = np.abs(model.coef_).flatten()
 
 # Pair the feature names with their importance values
@@ -46,16 +44,12 @@ feature_importance_dict = dict(zip(X.columns, feature_importances))
 # Sort the features based on importance
 sorted_features = sorted(feature_importance_dict.items(), key=lambda x: x[1], reverse=True)
 
-# Printing the results in a neat format
-print("\nVariables ranked by their importance in predicting student dropout:\n")
+# Printing the results
+print("\\nVariables ranked by their importance in predicting student dropout:\\n")
 for rank, (feature, importance) in enumerate(sorted_features, 1):
     print(f"{rank}. {feature}: {importance:.4f}")
-print("\n")
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Set up the figure size and grid layout
+# Visualization
 plt.figure(figsize=(20, 15))
 sns.set_style("whitegrid")
 
@@ -67,7 +61,6 @@ for index, column in enumerate(numerical_cols, 1):
     plt.tight_layout()
 
 plt.show()
-
 
 # Selected influential variables for visualization
 selected_vars = [
